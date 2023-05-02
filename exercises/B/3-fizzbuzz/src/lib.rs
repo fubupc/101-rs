b68041b2e3019fb62065b709de90e84db97ebe2c
Yao Fu <fubupc@gmail.com>
2023-05-03 00:38:11 +0800

    exercises/B3 FizBuzz

--- exercises/B/3-fizzbuzz/src/lib.rs
+++ exercises/B/3-fizzbuzz/src/lib.rs
@@ -1,15 +1,13 @@
 /// Very naive implementation of FizzBuzz
 pub fn fizz_buzz(i: u32) -> String {
-    if i % 3 == 0 {
-        if i % 5 == 0 {
-            "FizzBuzz".to_owned()
-        } else {
-            "Fizz".to_owned()
-        }
+    if i % 15 == 0 {
+        "FizzBuzz".to_owned()
+    } else if i % 3 == 0 {
+        "Fizz".to_owned()
     } else if i % 5 == 0 {
         "Buzz".to_owned()
     } else {
-        format!("{i}")
+        i.to_string()
     }
 }
 
@@ -17,3 +15,10 @@ pub fn fizz_buzz(i: u32) -> String {
 // to compare.
 // You can use the `include_str!()` macro to include file
 // contents as `&str` in your artifact.
+#[test]
+fn test_fizz_buzz() {
+    let out = include_str!("../fizzbuzz.out");
+    for (idx, line) in out.lines().enumerate() {
+        assert_eq!(fizz_buzz((idx + 1) as u32), line);
+    }
+}
